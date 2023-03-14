55920dcfbe6a32622e4e8524607584333fc5315d
Afrijal Dzuhri <ijuldz@gmail.com>
2023-03-14 19:29:36 +0700

    add index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,10 @@ import todos from "./todos.ts";
 import { Router } from "deps";
 
 const router = new Router();
+router.get("/", (ctx) => {
+  ctx.response.body = "bole";
+});
+
 router.use("/users", users.routes());
 router.use("/todos", todos.routes());
 
